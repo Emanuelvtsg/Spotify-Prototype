18ee6f8653f96a93414f08a792ed0c982d4f94ef
Emanuel Vinícius <evtsg@cin.ufpe.br>
2025-02-15 00:36:42 -0300

    adding content from lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Artists from './pages/artists'
+import Artist from './pages/artist'
+import Songs from './pages/songs'
+import Song from './pages/song'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-      <Main></Main>
-    </> 
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/artists' element={<Artists />}/>
+        <Route path='/artist/:id' element={<Artist/>}/>
+        <Route path='/songs' element={<Songs />}/>
+        <Route path='/song/:id' element={<Song/>}/>
+
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
